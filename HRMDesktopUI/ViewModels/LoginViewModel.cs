a105b61718e65b1b228e67b974bf36bf6f140e4f
hmandiv321 <hmandiv@outlook.com>
2019-11-11 01:59:43 -0500

    Add login form and shell menu design
    Add shellview menu and creates login view model and login view form

--- HRMDesktopUI/ViewModels/LoginViewModel.cs
+++ HRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Caliburn.Micro;
 
 namespace HRMDesktopUI.ViewModels
 {
-    //reason for using conductor ==> https://caliburnmicro.com/documentation/composition
-    public class ShellViewModel : Conductor<object>
+    class LoginViewModel
     {
     }
 }
